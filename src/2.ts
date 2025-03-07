0ca93703e84fd583f1708d254c3f80fa58e51332
mausbeckford863 <mausbeckford863@gmail.com>
2025-03-07 09:29:29 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,23 @@
+// MathClass.ts
+
+class MathClass {
+  constructor() {}
+
+  add(a: number, b: number): number {
+    return a + b;
+  }
+
+  subtract(a: number, b: number): number {
+    return a - b;
+  }
+
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+
+  divide(a: number, b: number): number {
+    return a / b;
+  }
+}
+
+export default MathClass;
