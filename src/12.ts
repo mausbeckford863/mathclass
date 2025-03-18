1e56bc0528812d0a02b659b91a2c17baa8518481
mausbeckford863 <mausbeckford863@gmail.com>
2025-03-18 17:44:33 +0000

    Add 12.ts

---
+++ src/12.ts
@@ -0,0 +1,16 @@
+.mathclass {
+  font-family: 'Roboto', sans-serif;
+}
+
+h1, h2, h3, h4 {
+  color: #007bff;
+}
+
+p {
+  margin-bottom: 1rem;
+}
+
+a {
+  color: #007bff;
+  text-decoration: none;
+}
