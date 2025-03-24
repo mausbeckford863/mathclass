ffd66fd921e4c04cb8bc901def5307201fb4a164
mausbeckford863 <mausbeckford863@gmail.com>
2025-03-24 11:16:53 +0000

    Add 17.ts

---
+++ src/17.ts
@@ -0,0 +1,8 @@
+let x = 5;
+if (x > 10) {
+    console.log("The value of x is greater than 10");
+} else if (x >= 8) {
+    console.log("The value of x is between 8 and 10");
+} else {
+    console.log("The value of x is less than or equal to 8");
+}
