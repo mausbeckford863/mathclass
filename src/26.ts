3e7d428bb38659c1c50b283a9fa05938220c1c77
mausbeckford863 <mausbeckford863@gmail.com>
2025-04-07 04:26:33 +0000

    Add 26.ts

---
+++ src/26.ts
@@ -0,0 +1,10 @@
+// Define your variable here
+let x = 5;
+
+if (x > 10) {
+    console.log('The number is greater than 10');
+} else if (x < 5) {
+    console.log('The number is less than 5');
+} else {
+    console.log('The number is between 5 and 10.');
+}
